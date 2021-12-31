b3f140a1a971ad5a68cb78ae997f99bf1538bbe6
Rahul Jagani <rjagani088@rku.ac.in>
2021-12-31 09:35:02 +0530

    Program in Proccess

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 var event = requlre ('event')
 var em = new event.EventEmitter();
+
+em.on('myEvent',function(date){
+    count=0;
+    date, forEach(i=> {
+     count = count + i;       
+    });
+    console.log(count);
+});
+em.emit('myEvent',[1,2,3,4]);
